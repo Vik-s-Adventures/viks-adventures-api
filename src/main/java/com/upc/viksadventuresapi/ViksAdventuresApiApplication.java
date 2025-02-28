c2538c3f5ac3796a993b0f7ba14cdb1ede7ec89c
Shayla Choque <u20201c144@upc.edu.pe>
2025-02-27 23:07:31 -0500

    Add shared folder

---
+++ src/main/java/com/upc/viksadventuresapi/ViksAdventuresApiApplication.java
@@ -0,0 +1,13 @@
+package com.upc.viksadventuresapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViksAdventuresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViksAdventuresApiApplication.class, args);
+	}
+
+}
