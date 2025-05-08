eeaf08e1cf075f8f30f8ae454e6539fb5ea8d312
Shayla Choque <u20201c144@upc.edu.pe>
2025-05-08 17:30:07 -0500

    Add adventure primary queries for services

--- src/main/java/com/upc/viksadventuresapi/adventure/domain/services/FinalBattleCommandService.java
+++ src/main/java/com/upc/viksadventuresapi/adventure/domain/services/FinalBattleCommandService.java
@@ -1,4 +1,12 @@
 package com.upc.viksadventuresapi.adventure.domain.services;
 
+import com.upc.viksadventuresapi.adventure.domain.model.aggregates.FinalBattle;
+import com.upc.viksadventuresapi.adventure.domain.model.commands.CreateFinalBattleCommand;
+import com.upc.viksadventuresapi.adventure.domain.model.commands.DeleteFinalBattleCommand;
+
+import java.util.Optional;
+
 public interface FinalBattleCommandService {
+    Optional<FinalBattle> handle(CreateFinalBattleCommand command);
+    void handle(DeleteFinalBattleCommand command);
 }
