7affa41499258c5c683dc98a8263cb981356b661
Shayla Choque <u20201c144@upc.edu.pe>
2025-06-05 18:01:56 -0500

    Fix matchingItem relationships

--- src/main/java/com/upc/viksadventuresapi/adventure/infrastructure/persistence/jpa/repositories/MatchingItemRepository.java
+++ src/main/java/com/upc/viksadventuresapi/adventure/infrastructure/persistence/jpa/repositories/MatchingItemRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 @Repository
 public interface MatchingItemRepository extends JpaRepository<MatchingItem, Long> {
+    List<MatchingItem> findByMatchingId(Long matchingId);
     List<MatchingItem> findByMatchingPairId(Long matchingPairId);
 }
--- src/main/java/com/upc/viksadventuresapi/adventure/interfaces/rest/resources/CreateMatchingItemResource.java
+++ src/main/java/com/upc/viksadventuresapi/adventure/interfaces/rest/resources/CreateMatchingItemResource.java
@@ -1,6 +1,7 @@
 package com.upc.viksadventuresapi.adventure.interfaces.rest.resources;
 
 public record CreateMatchingItemResource(
+        Long matchingId,
         Long matchingPairId,
         String imageUrl,
         boolean isDistractor
